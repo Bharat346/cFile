a8655c6e8d89ba22a5f5aa051cd4b2ba7251c9b7
Bharat kumar <139418302+Bharat346@users.noreply.github.com>
2024-03-17 00:37:42 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello world");
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
